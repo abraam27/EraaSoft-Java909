72b330182875097ac0bf4f92b397286fb55a25a0
Abram Emad <abram.emad@genwin.net>
2026-01-04 19:29:45 +0200

    JSP - Lecture 18 - Update form action in User Information Form to point to UserServlet

---
+++ JSP-Servlet/lecture18/src/main/java/UserServlet.java
@@ -0,0 +1,55 @@
+
+
+import jakarta.servlet.ServletException;
+import java.io.PrintWriter;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        String fullName = request.getParameter("fullName");
+        String age = request.getParameter("age");
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h2>User Details</h2>");
+        out.println("<p>Full Name: " + fullName + "</p>");
+        out.println("<p>Age: " + age + "</p>");
+        out.println("</body>");
+        out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
